78a4d178770a9239706bbe79deb9a5b4e303b35d
shepardb0y-src <60862129+shepardb0y-src@users.noreply.github.com>
2023-08-19 08:30:55 -0400

    created super user

--- frontend/src/pages/Homepage.js
+++ frontend/src/pages/Homepage.js
@@ -1,10 +1,14 @@
 import React from "react";
+import styled from "styled-components";
 
+const Container = styled.div`
+  background-color: red;
+`;
 const Homepage = () => {
   return (
-    <div>
+    <Container>
       <h1>Homepage</h1>
-    </div>
+    </Container>
   );
 };
 
--- music_controller/settings.py
+++ music_controller/settings.py
@@ -49,9 +49,10 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
+
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
@@ -59,7 +60,7 @@ MIDDLEWARE = [
 
 ]
 
-CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'music_controller.urls'
 
