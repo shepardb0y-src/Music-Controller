eaa78bff726a05113e5c04c9611a7ea0cd449085
shepardb0y-src <60862129+shepardb0y-src@users.noreply.github.com>
2023-08-27 08:14:57 -0400

    added two buttons

--- frontend/src/pages/CreateRoomPage.js
+++ frontend/src/pages/CreateRoomPage.js
@@ -3,13 +3,12 @@ import { Link } from "react-router-dom";
 import Button from "@mui/material/Button";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
-// import TextField from "@mui/material/TextField";
+import TextField from "@mui/material/TextField";
 import FormHelperText from "@mui/material/FormHelperText";
 import Radio from "@mui/material/Radio";
 import RadioGroup from "@mui/material/RadioGroup";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import FormControl from "@mui/material/FormControl";
-import FormLabel from "@mui/material/FormLabel";
 
 import styled from "styled-components";
 
@@ -48,6 +47,33 @@ const CreateRoomPage = () => {
               />
             </RadioGroup>
           </FormControl>
+        </Grid>
+
+        <Grid item xs={12} align="center">
+          <FormControl>
+            <TextField
+              required={true}
+              // defaultValue={this.defaultVotes}
+              inputProps={{
+                min: 1,
+                style: { textAlign: "center" },
+              }}
+            />
+            <FormHelperText>
+              <div align="center">Votes Required To Skip</div>
+            </FormHelperText>
+          </FormControl>
+        </Grid>
+
+        <Grid item xs={12} align="center">
+          <Button color="primary" variant="containec">
+            Create a Room
+          </Button>
+        </Grid>
+        <Grid item xs={12} align="center">
+          <Button color="secondary" variant="container" to="/" component={Link}>
+            Back
+          </Button>
         </Grid>
       </Grid>
     </Background>
