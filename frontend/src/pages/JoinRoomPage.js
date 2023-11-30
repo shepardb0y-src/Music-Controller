ec4d82761a23dad7e92ad27936cd2f78f6f6f88a
shepardb0y-src <60862129+shepardb0y-src@users.noreply.github.com>
2023-11-30 13:53:45 -0500

    i a began styling and added the state to the join rom component. adding am enter room and a functioning back button

--- frontend/src/pages/JoinRoomPage.js
+++ frontend/src/pages/JoinRoomPage.js
@@ -1,9 +1,46 @@
 import React from "react";
+import Button from "@mui/material/Button";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import TextField from "@mui/material/TextField";
+import FormHelperText from "@mui/material/FormHelperText";
+
+import { Link } from "react-router-dom";
+
+import { useState } from "react";
 
 const JoinRoomPage = () => {
+  const [roomCode, setRoomCode] = useState("");
+  const [error, setError] = useState("");
   return (
     <div>
-      <h1>Join</h1>
+      <Grid container spacing={1}>
+        <Grid item xs={12} align="center">
+          <Typography variant="h4" color="initial">
+            JOin a Room
+          </Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            // error={{ error }}
+            label="code"
+            placeholder="Enter Room COde"
+            // value={{ roomCode }}
+            // helperText={{ error }}
+            variant="outlined"
+          ></TextField>
+        </Grid>
+        <Grid item xs={12} align="center">
+          <Button color="primary" variant="contained" onClick>
+            Enter room
+          </Button>
+        </Grid>
+        <Grid item xs={12} align="center">
+          <Button color="secondary" variant="contained" to="/" component={Link}>
+            Back
+          </Button>
+        </Grid>
+      </Grid>
     </div>
   );
 };
