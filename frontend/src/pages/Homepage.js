94631a052107e4273fb68959c42737daf2d910fb
shepardb0y-src <60862129+shepardb0y-src@users.noreply.github.com>
2023-12-04 07:36:56 -0500

    I added the create a room button to the home page and joina room button as well that link to thier respective pages

--- frontend/src/pages/Homepage.js
+++ frontend/src/pages/Homepage.js
@@ -1,5 +1,12 @@
 import styled from "styled-components";
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import Button from "@mui/material/Button";
+import ButtonGroup from "@mui/material/ButtonGroup";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import TextField from "@mui/material/TextField";
+
 const Container = styled.div`
   background-color: yellow;
   height: 100%;
@@ -9,6 +16,17 @@ const Homepage = () => {
   return (
     <Container>
       <h1>Homepage</h1>
+      <ButtonGroup
+        variant="contained"
+        aria-label="outlined primary button group"
+      >
+        <Button to="/join" component={Link}>
+          Join a Room
+        </Button>
+        <Button to="/create" component={Link}>
+          Create a Rooom
+        </Button>
+      </ButtonGroup>
     </Container>
   );
 };
