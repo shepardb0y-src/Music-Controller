6f1b3d7ae0b4c7fda77c88b63212fd3099d120c4
shepardb0y-src <60862129+shepardb0y-src@users.noreply.github.com>
2023-12-11 17:49:10 -0500

    I added the userinroom view to track if a user has a sssoin made already

--- api/views.py
+++ api/views.py
@@ -4,9 +4,11 @@ from .serializers import RoomSerializer, CreateRoomSerializer
 from .models import Room
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from django.http import JsonResponse
 
 
 # Create your views here.
+
 
 
 class RoomView(generics.ListAPIView):
@@ -45,7 +47,7 @@ class JoinRoom(APIView):
                 room = room_result[0]
                 self.request.session['room_code'] = code
                 return Response({'message': 'Room Joined!'}, status=status.HTTP_200_OK)
-
+ 
             return Response({'Bad Request': 'Invalid Room Code'}, status=status.HTTP_400_BAD_REQUEST)
 
         return Response({'Bad Request': 'Invalid post data, did not find a code key'}, status=status.HTTP_400_BAD_REQUEST)
@@ -77,3 +79,15 @@ class CreateRoomView(APIView):
                 return Response(RoomSerializer(room).data, status=status.HTTP_201_CREATED)
 
         return Response({'Bad Request': 'Invalid data...'}, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+class UserInRoom(APIView):
+    def get(self, request, format=None):
+        if not self.request.session.exists(self.request.session.session_key):
+            self.request.session.create()
+
+        data = {
+            'code': self.request.session.get('room_code')
+        }
+        return JsonResponse(data, status=status.HTTP_200_OK)
--- frontend/src/components/Room.js
+++ frontend/src/components/Room.js
@@ -6,49 +6,50 @@ const Room = () => {
   const [guestCanPause, setguestCanPause] = useState(true);
   const [votesToSkip, setvotesToSkip] = useState(0);
   const [isHost, setHost] = useState(false);
-
+  
   const roomcode = useMatch("/room/:roomcode");
   //   const roomcode = props.match.params;
   // Access params from the matched URL
   // params is a special attribute connted to the router hook
-
+  
   /// convert to axios
   // currently http://127.0.0.1:8000/room/KNOPPE is giving a 404 error on GET request from
   // const getRoomDetails = () => {
-  //   fetch("/get-room" + "?code=" + roomcode.params.roomcode).then((response) =>
-  //     response.json().then((data) => setvotesToSkip(data.votes_to_skip))
-  //   );
-  // };g
-  // const getRoomDetails = () => {
-  //   axios
-  //     .get("/get-room" + "?code=" + roomcode.params.roomcode) //returns the data from api differ from fetch by not need another
-  //     .then((response) => response)
-  //     .then((res) =>
-  //       setvotesToSkip({
-  //         votesToSkip: res.data.votes_to_skip,
-  //       })
-  //     );
-
-  //   console.log(roomcode);
-  // };
-  // getRoomDetails();
-
-  const fetchData = async () => {
-    const response = await axios.get(
-      "/get-room" + "?code=" + roomcode.params.roomcode
-    );
-    const data = response.data;
-    console.log(data);
-    setvotesToSkip(data.votes_to_skip);
-    setHost(data.is_host.toString());
-    setguestCanPause(data.guest_can_pause.toString());
-  };
-
-  useEffect(() => {
-    fetchData();
-  }, []);
-  return (
-    <div>
+    //   fetch("/get-room" + "?code=" + roomcode.params.roomcode).then((response) =>
+    //     response.json().then((data) => setvotesToSkip(data.votes_to_skip))
+    //   );
+    // };g
+    // const getRoomDetails = () => {
+      //   axios
+      //     .get("/get-room" + "?code=" + roomcode.params.roomcode) //returns the data from api differ from fetch by not need another
+      //     .then((response) => response)
+      //     .then((res) =>
+      //       setvotesToSkip({
+        //         votesToSkip: res.data.votes_to_skip,
+        //       })
+        //     );
+        
+        //   console.log(roomcode);
+        // };
+        // getRoomDetails();
+        useEffect(() => {
+          fetchData(); g
+          console.log(`didMount:`);
+        }, []);
+        
+        const fetchData = async () => {
+          const response = await axios.get(
+            "/get-room" + "?code=" + roomcode.params.roomcode
+            );
+            const data = response.data;
+            console.log(data);
+            setvotesToSkip(data.votes_to_skip);
+            setHost(data.is_host.toString());
+            setguestCanPause(data.guest_can_pause.toString());
+          };
+          
+          return (
+            <div>
       <h3>{roomcode.params.roomcode}</h3>
       <p>Votes:{votesToSkip}</p>
       {console.log(guestCanPause)}
