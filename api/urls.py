5bddc1b2e79c0ddd89501e46c5d8766b140fa932
shepardb0y-src <60862129+shepardb0y-src@users.noreply.github.com>
2023-12-11 17:54:42 -0500

    i added the user in room url to the urls.

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,6 @@
 
 from django.urls import path
-from .views import RoomView, CreateRoomView, GetRoom, JoinRoom
+from .views import RoomView, CreateRoomView, GetRoom, JoinRoom, UserInRoom
 
 urlpatterns = [
 
@@ -8,4 +8,5 @@ urlpatterns = [
     path('create-room', CreateRoomView.as_view()),
     path('get-room', GetRoom.as_view()),
     path('join-room', JoinRoom.as_view()),
+    path('user-in-room', UserInRoom.as_view()),
 ]
